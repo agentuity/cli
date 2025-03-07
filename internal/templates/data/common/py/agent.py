801ae7853c4d0e905fbd31d0892c5591fbf0d83a
Bobby Christopher <b.christopher.3rd@gmail.com>
2025-03-06 21:45:17 -0500

    added types to other file

--- internal/templates/data/common/py/agent.py
+++ internal/templates/data/common/py/agent.py
@@ -1,2 +1,3 @@
-def run(request, response, context):
+from agentuity.server.types import AgentRequest, AgentResponse, AgentContext
+def run(request: AgentRequest, response: AgentResponse, context: AgentContext):
     return response.text("Hi there!")
