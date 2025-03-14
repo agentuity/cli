bdaf46267bd96867b783bfb4d3c86c21d6d67dfc
Jeff Haynie <jhaynie@gmail.com>
2025-03-14 18:56:00 -0500

    more clear error / warning messages

--- internal/templates/data/common/js/boot.ts
+++ internal/templates/data/common/js/boot.ts
@@ -1,8 +1,18 @@
 import { runner } from "@agentuity/sdk";
 
 if (!process.env.AGENTUITY_API_KEY) {
-	console.error("AGENTUITY_API_KEY is not set. This should have been set automatically by the Agentuity CLI or picked up from the .env file.");
+	console.error("\x1b[31m[ERROR] AGENTUITY_API_KEY is not set. This should have been set automatically by the Agentuity CLI or picked up from the .env file.\x1b[0m");
 	process.exit(1);
+}
+
+if (!process.env.AGENTUITY_URL) {
+	console.warn("\x1b[31m[WARN] You are running this agent outside of the Agentuity environment. Any automatic Agentuity features will be disabled.\x1b[0m");
+	if (process.isBun) {
+		console.warn("\x1b[31m[WARN] Recommend running `agentuity dev` to run your project locally instead of bun run start.\x1b[0m");
+	} else {
+		console.warn("\x1b[31m[WARN] Recommend running `agentuity dev` to run your project locally instead of npm start.\x1b[0m");
+	}
+	console.log(process.env);
 }
 
 runner(true, import.meta.dirname).catch((err) => {
