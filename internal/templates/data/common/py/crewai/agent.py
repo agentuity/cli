3dbbdf5a45fc94dbce1e5f9e17e12e6fc82aa22f
Jeff Haynie <jhaynie@agentuity.com>
2025-03-16 17:27:58 -0500

    Fix change in signature with request.text -> request.data.text (#100)

--- internal/templates/data/common/py/crewai/agent.py
+++ internal/templates/data/common/py/crewai/agent.py
@@ -3,6 +3,6 @@ from agents.{{ .AgentName | safe_filename }}.crew import MyCrew
 
 
 async def run(request: AgentRequest, response: AgentResponse, context: AgentContext):
-    inputs = {"topic": request.text or "AI LLMs"}
+    inputs = {"topic": request.data.text or "AI LLMs"}
     result = MyCrew().crew().kickoff(inputs=inputs)
     return response.text(str(result))
--- internal/templates/data/common/py/litellm.py
+++ internal/templates/data/common/py/litellm.py
@@ -3,6 +3,6 @@ from agentuity import AgentRequest, AgentResponse, AgentContext
 
 
 async def run(request: AgentRequest, response: AgentResponse, context: AgentContext):
-    messages = [{"content": request.text or "Hello, how are you?", "role": "user"}]
+    messages = [{"content": request.data.text or "Hello, how are you?", "role": "user"}]
     result = completion(model="openai/gpt-4o", messages=messages)
     return response.text(result.choices[0].message.content)
