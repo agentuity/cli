230dde5bdd46ee091bd6d5d6055ce72a51d115e5
Jeff Haynie <jhaynie@agentuity.com>
2025-04-25 09:23:43 -0500

    Revert "temporarily comment out the new sdk upgrade requirement until ready" (#229)
    * Revert "temporarily comment out the new sdk upgrade requirement until ready"
    
    This reverts commit e7de330f930b6c91797f031a9be09e369a080734.
    
    * remove used variable

--- internal/bundler/upgrade.go
+++ internal/bundler/upgrade.go
@@ -19,18 +19,18 @@ type breakingChange struct {
 }
 
 var jsBreakingChanges = []breakingChange{
-	// {
-	// 	Runtime: "bunjs",
-	// 	Version: "<0.0.106",
-	// 	Title:   "🚫 JS SDK Breaking Change 🚫",
-	// 	Message: "The JS SDK type signatures for AgentRequest have changed to be async functions. Please see the v0.0.106 Changelog for how to update your code.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00106") + "\n\nPlease bun upgrade @agentuity/sdk, fix your types and ensure your code passes type checking and then re-run this command again.",
-	// },
-	// {
-	// 	Runtime: "nodejs",
-	// 	Version: "<0.0.106",
-	// 	Title:   "🚫 JS SDK Breaking Change 🚫",
-	// 	Message: "The JS SDK type signatures for AgentRequest have changed to be async functions. Please see the v0.0.106 Changelog for how to update your code.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00106") + "\n\nPlease npm upgrade @agentuity/sdk, fix your types and ensure your code passes type checking and then re-run this command again.",
-	// },
+	{
+		Runtime: "bunjs",
+		Version: "<0.0.106",
+		Title:   "🚫 JS SDK Breaking Change 🚫",
+		Message: "The JS SDK type signatures for AgentRequest have changed to be async functions. Please see the v0.0.106 Changelog for how to update your code.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00106") + "\n\nPlease bun upgrade @agentuity/sdk, fix your types and ensure your code passes type checking and then re-run this command again.",
+	},
+	{
+		Runtime: "nodejs",
+		Version: "<0.0.106",
+		Title:   "🚫 JS SDK Breaking Change 🚫",
+		Message: "The JS SDK type signatures for AgentRequest have changed to be async functions. Please see the v0.0.106 Changelog for how to update your code.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00106") + "\n\nPlease npm upgrade @agentuity/sdk, fix your types and ensure your code passes type checking and then re-run this command again.",
+	},
 }
 
 type packageJSON struct {
