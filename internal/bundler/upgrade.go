16196d5fd373f930a6fa7f283365b3fea8ccdd5e
Bobby Christopher <b.christopher.3rd@gmail.com>
2025-05-13 13:33:40 -0400

    added version

--- internal/bundler/upgrade.go
+++ internal/bundler/upgrade.go
@@ -44,7 +44,7 @@ var breakingChanges = []breakingChange{
 		Runtime: "bunjs",
 		Version: "<0.0.115",
 		Title:   "🚫 JS SDK Breaking Change 🚫",
-		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.114 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00114") + "\n\nAfter migrated, please run bun update @agentuity/sdk --latest and then re-run this command again.",
+		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.115 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00114") + "\n\nAfter migrated, please run bun update @agentuity/sdk --latest and then re-run this command again.",
 		Callback: func(ctx BundleContext) error {
 			files := []string{
 				filepath.Join(ctx.ProjectDir, "index.ts"),
@@ -74,7 +74,7 @@ var breakingChanges = []breakingChange{
 		Runtime: "nodejs",
 		Version: "<0.0.115 ",
 		Title:   "🚫 JS SDK Breaking Change 🚫",
-		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.114 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00114") + "\n\nAfter migrated, please run npm i @agentuity/sdk --latest and then re-run this command again.",
+		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.115 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00114") + "\n\nAfter migrated, please run npm i @agentuity/sdk --latest and then re-run this command again.",
 		Callback: func(ctx BundleContext) error {
 			files := []string{
 				filepath.Join(ctx.ProjectDir, "index.ts"),
@@ -104,7 +104,7 @@ var breakingChanges = []breakingChange{
 		Runtime: "uv",
 		Version: "<0.0.84",
 		Title:   "🚫 Python SDK Breaking Changes 🚫",
-		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.114 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/sdk-js#v00114") + "\n\nAfter migrated, please run `uv add agentuity -U` --latest and then re-run this command again.",
+		Message: "The environment variable and code reference for your Agentuity API key has changed from AGENTUITY_API_KEY to AGENTUITY_SDK_KEY. Update all occurrences in your .env files and codebase. See the v0.0.84 Changelog for details.\n\n" + tui.Link("https://agentuity.dev/Changelog/python-js#v0084") + "\n\nAfter migrated, please run `uv add agentuity -U` --latest and then re-run this command again.",
 		Callback: func(ctx BundleContext) error {
 			files := []string{
 				filepath.Join(ctx.ProjectDir, "server.py"),
