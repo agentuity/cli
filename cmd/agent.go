25bd8fc1f2ac81ae9278ff69519bca7868af3264
Pedro Enrique <pedro.tma@gmail.com>
2025-05-14 15:55:30 +0200

    Use local flag

--- cmd/agent.go
+++ cmd/agent.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -13,6 +15,7 @@ import (
 	"syscall"
 
 	"github.com/agentuity/cli/internal/agent"
+	"github.com/agentuity/cli/internal/dev"
 	"github.com/agentuity/cli/internal/errsystem"
 	"github.com/agentuity/cli/internal/project"
 	"github.com/agentuity/cli/internal/templates"
@@ -711,6 +714,7 @@ var agentTestCmd = &cobra.Command{
 
 		agentID, _ := cmd.Flags().GetString("agent-id")
 		payload, _ := cmd.Flags().GetString("payload")
+		local, _ := cmd.Flags().GetBool("local")
 
 		if agentID == "" {
 			keys, state := reconcileAgentList(logger, cmd, theproject.APIURL, theproject.Token, theproject)
@@ -738,18 +742,43 @@ var agentTestCmd = &cobra.Command{
 		if err != nil {
 			errsystem.New(errsystem.ErrApiRequest, err, errsystem.WithContextMessage("Failed to get agent API key")).ShowErrorAndExit()
 		}
+		endpoint := fmt.Sprintf("%s/webhook/%s", theproject.TransportURL, agentID)
+		if local {
+			port, _ := dev.FindAvailablePort(theproject)
+			endpoint = fmt.Sprintf("http://localhost:%d/agent_%s", port, agentID)
+		}
 
-		client := util.NewAPIClient(ctx, logger, theproject.TransportURL, apikey)
-		var resp agent.Response[any]
-		if err := client.Do("POST", fmt.Sprintf("/webhook/%s", agentID), strings.NewReader(payload), &resp); err != nil {
+		// use http package to send a POST request to the agent
+		req, err := http.NewRequest("POST", endpoint, strings.NewReader(payload))
+		if err != nil {
+			logger.Fatal("Failed to create request: %s", err)
+		}
+		// check if payload is json
+		if json.Valid([]byte(payload)) {
+			req.Header.Set("Content-Type", "application/json")
+		} else {
+			req.Header.Set("Content-Type", "application/octet-stream")
+		}
+		if apikey != "" {
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apikey))
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
 			logger.Fatal("Failed to send request: %s", err)
 		}
-		fmt.Println("Success:", resp.Success)
-		fmt.Println("Message:", resp.Message)
-		if sessionID, ok := resp.Data.(map[string]interface{})["sessionId"]; ok {
-			fmt.Println("Session ID:", sessionID)
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			logger.Fatal("Failed to read response: %s", err)
 		}
-
+		var jsonBody map[string]interface{}
+		if json.Unmarshal(body, &jsonBody) == nil {
+			stringified, _ := json.MarshalIndent(jsonBody, "", "  ")
+			tui.ShowSuccess("Agent Test: %s", tui.Paragraph(tui.Bold(string(stringified))))
+		} else {
+			tui.ShowSuccess("Agent Test: %s", tui.Paragraph(tui.Bold(string(body))))
+		}
 	},
 }
 
@@ -762,6 +791,7 @@ func init() {
 
 	agentTestCmd.Flags().String("agent-id", "", "The ID of the agent to test")
 	agentTestCmd.Flags().String("payload", "", "The payload to send to the agent")
+	agentTestCmd.Flags().Bool("local", false, "Enable local testing")
 
 	agentCmd.AddCommand(agentTestCmd)
 
